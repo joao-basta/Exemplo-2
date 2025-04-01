db431c5310d327e75951291d8addf633aa9b68c8
Joao Paulo <bpjoao00@gmail.com>
2025-04-01 09:27:48 -0300

    adicionando conversões

--- script.js
+++ script.js
@@ -17,3 +17,34 @@ console.log(valor)
 let exemplo={};
 
 let exemplo2= [];
+
+// tipos variaveis
+let exemplo3=10;
+console.log(typeof exemplo3)
+
+let exemplo4="aula";
+console.log(typeof exemplo4)
+
+let exemplo5=true
+console.log(typeof exemplo5)
+
+let exemplo6=["um", "dois", "tres"];
+console.log(typeof exemplo6)
+
+//conversões
+// float > inteiro
+let numfloat=123.456
+console.log(parseInt(numfloat));
+
+// string > Float
+
+let numsting ="546.654"
+console.log(parseFloat(numsting))
+
+// float > string
+
+let numfloat1=554.564
+console.log(numfloat1.toString());
+
+let numint=100;
+console.log(numint.toString())
