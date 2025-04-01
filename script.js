800ae2fa32787e47459a32721b0995dc27c64f5a
Joao Paulo <bpjoao00@gmail.com>
2025-04-01 09:10:31 -0300

    declaração e variaveis

---
+++ script.js
@@ -0,0 +1,19 @@
+//DECLARAÇÕES E VARIAVEIS
+var nome ="fiap";
+console.log(nome)
+
+let idade =18;
+console.log(idade);
+
+const sobrenome ="Basta";
+console.log(sobrenome)
+
+let aula
+console.log(aula)
+
+let valor=null;
+console.log(valor)
+
+let exemplo={};
+
+let exemplo2= [];
